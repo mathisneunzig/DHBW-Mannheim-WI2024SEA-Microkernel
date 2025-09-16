81a8fad1d90c2f685b30856ccbb5546b0fcf6554
I750962 <lea.dennhardt@sap.com>
2025-09-16 13:40:14 +0200

    playlist & profil first idea vorlesung

---
+++ src/plugins/LeaDennhardt_Playlist/index.ts
@@ -0,0 +1,14 @@
+/*import { pluginManager } from "../../app/pluginManager";
+import { PlaylistPage } from "./page";
+
+pluginManager.register({
+  id: "Playlist",
+  route: "Playlist",
+  component: PlaylistPage,
+  tile: "Playlist",
+  color1: "FloralWhite",
+  color2: "ForestGreen",
+  spin: 45,
+  permissions: ["users.read"]
+});
+/*
---
+++ src/plugins/LeaDennhardt_Profil/index.ts
@@ -0,0 +1,13 @@
+import { pluginManager } from "../../app/pluginManager";
+import Profil from "./page";
+
+pluginManager.register({
+  id: "Profil",
+  route: "Profil",
+  component: Profil,
+  tile: "Profil",
+  color1: "DarkOliveGreen",
+  color2: "AntiqueWhite",
+  spin: 45, 
+  permissions: ["profileImage.read", "profileImage.write"]
+});
